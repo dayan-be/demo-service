3c368aeaa56757c189b84764f045fceaaf22c42f
EvanPan <EvanPan@boyaa.com>
2018-11-09 21:22:24 +0800

    feat(demo):first commit

---
+++ global/config.go
@@ -0,0 +1,52 @@
+package global
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+
+type Configs struct {
+	Srv struct {
+		SrvName string `yaml:"srvName"`
+		SrvId   uint32 `yaml:"srvId"`
+		Addr    string `yaml:"addr"`
+		Version string `yaml:"version"`
+	}
+
+	Log struct {
+		LogLevel    int  `yaml:"logLevel"`
+		LogFileSize int  `yaml:"logFileSize"`
+		JsonFile    bool `yaml:"jsonFile"`
+	}
+}
+
+var cfgIns *Configs
+
+func Config() *Configs {
+	return cfgIns
+}
+
+func LoadConfig(filename string) *Configs {
+
+	buff, err := ioutil.ReadFile(filename)
+	if err != nil {
+		goto FAILED
+	}
+
+	err = yaml.Unmarshal(buff, cfgIns)
+	if err != nil {
+		goto FAILED
+	}
+	return cfgIns
+
+FAILED:
+	fmt.Printf("failed:%v",err)
+	os.Exit(1)
+}
+
+func init() {
+	cfgIns = &Configs{}
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/dayan-be/demo-service/global"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	GitTag    = "2000.01.01.release"
+	BuildTime = "2000-01-01T00:00:00+0800"
+)
+
+func main() {
+
+	//显示版本号信息　
+	version := flag.Bool("v", false, "version")
+	flag.Parse()
+
+	if *version {
+		fmt.Println("Git Tag: " + GitTag)
+		fmt.Println("Build Time: " + BuildTime)
+		return
+	}
+
+	//1.load configer
+	cfg := global.LoadConfig("config.yaml")
+
+	//2.log
+	logrus.SetLevel(logrus.DebugLevel)
+
+
+
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
+	<-c
+	os.Exit(0)
+}
